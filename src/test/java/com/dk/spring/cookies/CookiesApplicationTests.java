209840f45147d6ab07ad5b47007cd572aedcec17
Dávid Kecskeméti <dxdy421@gmail.com>
2020-10-11 16:20:53 +0200

    Initial commit

---
+++ src/test/java/com/dk/spring/cookies/CookiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dk.spring.cookies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
